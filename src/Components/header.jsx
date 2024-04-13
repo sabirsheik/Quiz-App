656f7bdacb90f707957c798399330c18115ffcc2
SABIR ALI <sabirsheik12787@gmail.com>
2024-04-13 23:36:41 +0500

    mini chg

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
     <header className="d-f-a">
         <img src="../public/quiz.png" alt="" />
         <h1>Quiz App</h1>
-        <button><h3><a href="">Reset Quiz</a></h3></button>
+        <button><h3><a href="App.jsx">Reset Quiz</a></h3></button>
     </header>
     </>
   )
